50a2195ea66ba6bf6cb3aea2c125b9aeec3e5be3
Tam Huynh <tamdavide.huynh@gmail.com>
2022-01-24 22:57:43 +0100

    Implement button events callbacks (forward, backward, pause)

--- src/sherlock.py
+++ src/sherlock.py
@@ -22,53 +22,59 @@ class Sherlock:
                  PLAY_PIN=11, 
                  OUT_PIN=13,
                  BOUNCE=200, # [milliseconds]
-                 SKIP=10, # [seconds]
+                 SKIP_TIME=10, # [seconds]
                  LONG_PRESS_TIME=2, # [seconds]
-                 START_IDX=0, 
-                 PLAY_STATE=False
+                 CURRENT_IDX=0, 
+                 PLAY_STATE=False,
+                 RESTART_TIME=2 # [seconds]
                  ):
         '''
-        Args:
-            FW_PIN (int)         : RaspberryPi board pin for NEXT button
-            BW_PIN (int)         : RaspberryPi board pin for BACK button
-            PLAY_PIN (int)       : RaspberryPi board pin for PLAY/PAUSE button
-            OUT_PIN (int)        : RaspberryPi board pin for OUTPUT
-            BOUNCE (int)         : time delay (in [ms]) to compensate bounce effect
-            SKIP (int)           : time (in [s]) to skip when long-pressing the NEXT button
-            LONG_PRESS_TIME (int): time (in [s]) to long-press the NEXT button before triggering the fast-forward
-            START_IDX (int)      : index of track to start the playback (assuming tracks are ordered)
-            PLAY_STATE (bool)    : flag for play/pause status
+        Store all parameters, then (1) initialize the board, (2) initialize
+        te pygame.mixer.music object.
         
+        Args:
+            TRACKS_DIR (str)        : path to the folder where the tracks are stored
+            FW_PIN (int)            : RaspberryPi board pin for NEXT button
+            BW_PIN (int)            : RaspberryPi board pin for BACK button
+            PLAY_PIN (int)          : RaspberryPi board pin for PLAY/PAUSE button
+            OUT_PIN (int)           : RaspberryPi board pin for OUTPUT
+            BOUNCE (int)            : time delay (in [ms]) to compensate bounce effect
+            SKIP_TIME (int)         : time (in [s]) to skip when long-pressing the NEXT button
+            LONG_PRESS_TIME (int)   : time (in [s]) to long-press the NEXT button before triggering the fast-forward
+            CURRENT_IDX (int)       : index of track to start the playback (assuming tracks are ordered)
+            PLAY_STATE (bool)       : flag for play/pause status
+            RESTART_TIME (int)      : time (in [s]) AFTER which the BACK button restarts the current track instead of going back to the previous one
         '''
         # RaspberryPi board setup
         self.fw_pin = FW_PIN # NEXT
         self.bw_pin = BW_PIN # BACK
         self.play_pin = PLAY_PIN # PLAY/PAUSE
         self.out_pin = OUT_PIN # OUTPUT
+        self.bounce = BOUNCE # Compensate bounce effect
         
-        # Compensate bounce effect
-        self.bounce = BOUNCE
+        # Other parameters
+        self.tracks = [os.path.join(TRACKS_DIR, track) for track in os.listdir(TRACKS_DIR)] # store tracks
+        self.skip_time = SKIP_TIME # How many seconds to skip forward when long-pressing NEXT button
+        self.long_press_time = LONG_PRESS_TIME # How long to keep pressing the NEXT button to trigger the fast-forward
+        self.current_idx = CURRENT_IDX # From which track to start the playback (assuming the tracks in the folder are ordered)
+        self.is_playing = PLAY_STATE # Flag for play/pause status
+        self.restart_track_time = RESTART_TIME # after how many seconds the BACK button pressing restarts the current track instead of going back to the previous track
         
-        # Initialize GPIO   
+        ### INITIALIZATIONS ### 
+        # 1. Initialize GPIO 
+        print('Initializing board...')
         self.init_board()
+        # 2. Initialize audioplayer
+        print('Initializing audioplayer...')
+        self.init_player()
         
-        # Store tracks
-        self.tracks = [os.path.join(TRACKS_DIR, track) for track in os.listdir(TRACKS_DIR)]
-        # How many seconds to skip forward when long-pressing NEXT button
-        self.fforward_skip = SKIP
-        self.long_press_time = LONG_PRESS_TIME
-        # From which track to start the playback (assuming the tracks in the folder are ordered)
-        self.start_idx = START_IDX
-        # Flag for play/pause status
-        self.is_playing = PLAY_STATE
-        
-        # Initialize pygame.mixer object
-        self.player = pygame.mixer.init().music
+        print('Ready! Press the PLAY/PAUSE button to start!')
         
     def init_board(self):
         '''Initialize GPIO input/output pins and event detection.'''
         ### GPIO SETUP ###
         GPIO.setmode(GPIO.BOARD)
+        
         # Set pins modes
         GPIO.setup(self.fw_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.bw_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -83,10 +89,132 @@ class Sherlock:
         
     def init_player(self):
         '''Initialize pygame.mixer object.'''
+        ### PYGAME SETUP ###
         self.mixer = pygame.mixer
         self.mixer.init()
-        
         self.player = self.mixer.music
         
-    
+    def _play(self):
+        '''Play the current track (self.current_idx).'''
+        # Stop the playback if it's playing
+        self.player.stop()
+        # Load the current track
+        self.player.load(self.tracks[self.current_idx])
+        # Start playing
+        self.player.play()
         
+        # Set is_playing flag
+        self.is_playing = True
+        
+    def _forward(self):
+        '''
+        Pressing the NEXT button lets the user skip to the next track.
+        Long-pressing the NEXT button for `N` seconds lets the user fast-forward the track by `skip` seconds.
+        '''
+        # Detect long-press
+        long_press_flag = self._long_press(self.fw_pin, fforward=True)
+            
+        # If single, short press, skip to next track
+        if (not long_press_flag):
+            if(self.current_idx == len(self.tracks)-1):
+                self.current_idx = 0
+            else:
+                self.current_idx += 1
+            # Play next track
+            self._play()
+
+            print("Avanti: " , self.current_idx)
+    
+    def _fastforward(self):
+        '''
+        Long-pressing the NEXT button for `N` seconds lets the user 
+        fast-forward the current track by `self.skip_time` seconds.
+        
+        Note: depending on the audio format, pygame.mixer.music.set_pos
+        behaves differently. For .mp3 audio files, set_pos sets the new
+        position relatively to the current position - i.e., if you do
+        set_pos(5), it will skip to 5 seconds after the current position.
+        
+        More info on the official documentation: 
+        https://www.pygame.org/docs/ref/music.html#pygame.mixer.music.set_pos
+        '''
+        # Skip by 'fforward_skip' seconds
+        self.player.set_pos(self.skip_time)
+    
+    def _backward(self):
+        '''
+        Pressing the BACK button lets te user either restart the current track
+        or go back to the previous one if the time from the start of the current
+        track is less than a pre-defined time interval.
+        
+        Note: get_pos() returns time from start of playback in [milliseconds].
+        '''
+        # Detect long-press
+        long_press_flag = self._long_press(self.bw_pin, fforward=False)
+        
+        if (not long_press_flag):
+            # If self.restart_track_time has passed, then restart current track
+            if(self.player.get_pos()//1000 > self.restart_track_time):
+                self._play()
+            else:
+                # Go to previous track
+                if(self.current_idx == 0):
+                    self.current_idx = len(self.tracks)-1
+                else:
+                    self.current_idx -= 1
+                self._play()
+                print("Indietro: " , self.current_idx)
+                
+    def _fastbackward(self):
+        '''
+        Long-pressing the BACK button for 'self.long_press_time' seconds
+        lets the user go back of the current track by 'self.skip_time' seconds.
+        
+        Note: see self._fastforward() note.
+        '''
+        # Go back by 'self.skip_time' seconds
+        raise NotImplementedError
+        
+    def _play_pause(self):
+        '''
+        Pressing the center PLAY/PAUSE button, plays or pauses the current track.
+        '''
+        if(self.is_playing):
+            self.player.pause()		
+            print("Pausa")
+            self.is_playing = False
+        else:
+            self.player.unpause()
+            print("Play")
+            self.is_playing = True
+            
+    def _long_press(self, pin, fforward):
+        '''
+        Detect long-pressing of any button.
+        
+        Args:
+            pin (int)       : pin to detect the long-pressing from
+            fforward (bool) : whether to fast-forward or fast-backward
+            
+        Returns:
+            long_press_flag (bool)  : whether the long-press was detected or not
+        '''
+        # Compute time from when button press is detected
+        start_press = time.time()
+        
+        # Detect if the NEXT button has been long-pressed
+        long_press_flag = False
+        
+        while GPIO.input(pin) == GPIO.LOW: 
+            time.sleep(.5)
+            if(time.time()-start_press > self.long_press_time):
+                # Fast for-/back-ward the current track by N seconds
+                if fforward==True: 
+                    self._fastforward()
+                elif fforward==False:
+                    self._fastbackward()
+                
+                # Do not go to the next track
+                long_press_flag = True
+                
+        return long_press_flag
