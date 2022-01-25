5d69571be845eeedcc6a8b18af75d9af5251f99c
Tam Huynh <tamdavide.huynh@gmail.com>
2022-01-25 21:53:17 +0100

    Add try-except block to handle exceptions and KeyboardInterrupt

--- src/main.py
+++ src/main.py
@@ -1,21 +1,13 @@
-import glob
-import json
 import os
-import sys
-import time
 import yaml
-from time import sleep
-
-import pygame
-import RPi.GPIO as GPIO
-from gpiozero import Button
 
 from sherlock import Sherlock
 
-if __name__=='__main__':
+def welcome_sherlock():
+    '''Prints welcome and instructions for usage.'''
     # Print instructions
     print('#'*10)
-    print('WELCOME TO SHERLOCK')
+    print('WELCOME TO SHERLOCK! :)')
     print('#'*10)
     print('\n')
     print('#'*5)
@@ -28,7 +20,16 @@ if __name__=='__main__':
     print('5. Long press the left button (BACK) to fast-backward the current track.')
     print('\n')
     print('Have fun!')
+
+def goodbye_sherlock():
+    '''Prints closing message for KeyboardInterrupt exception.'''
+    # Print goodbye
+    print('#'*10)
+    print('GOODBYE! COME BACK SOON! :)')
+    print('#'*10)
     
+def main():
+    '''Starts main loop by initializing the Sherlock device.'''
     # Load parameters file
     with open('./config/sherlock_parameters.yaml', 'r') as param_file:
         sherlock_params_dict = yaml.load(param_file, Loader=yaml.FullLoader)
@@ -36,6 +37,24 @@ if __name__=='__main__':
     # Initialize the device
     sherlock = Sherlock(**sherlock_params_dict)
     
-    # Start continuous loop
+    # Continue listening to events
     while True:
-        pass
+        pass    
+
+if __name__=='__main__':
+    '''
+    Wraps main loop in a try-except exception handling to catch 
+    KeyboardInterrupt as quit event.
+    '''
+    try:    
+        # Welcome message and instructions
+        welcome_sherlock()
+        # Start main loop
+        main()
+    # Catch CTRL+C command for quitting
+    except KeyboardInterrupt:
+        # Say goodbye
+        goodbye_sherlock()
+    # Deal elegantly with other errors and quit
+    except Exception as ex:
+        print('Encountered the following error: ', ex)
