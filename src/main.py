adb1889f87254e177d775557fdc5d555942c1740
Tam Huynh <tamdavide.huynh@gmail.com>
2023-06-18 21:53:33 +0200

    Move main run logic to Sherlock.run()

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
-import RPi.GPIO as GPIO
-import time
-
 from sherlock import Sherlock
 from utils import get_logger, goodbye_sherlock, read_yaml, welcome_sherlock
 
 
 logger = get_logger(__name__)
-    
 
 def main():
     """Starts main loop by initializing the Sherlock device."""
@@ -17,77 +13,24 @@ def main():
         # Load configuration parameters
         # TODO: Make path non-hardcoded
         sherlock_params_dict = read_yaml()
+        # Add logger
+        sherlock_params_dict.update({"logger": logger})
         
         # Initialize the device
         sherlock = Sherlock(**sherlock_params_dict)
-        # Continue listening to events
-        long_press_flag = False
-        t_old = 0
-        while True:
-            # Check volume level
-            new_volume = round(sherlock.potentiometer.value, 1)
-            if(new_volume != sherlock.prev_volume):
-                sherlock._set_volume(new_volume)
-
-            # Forward press = next track
-            # Forward long-press = fast-forward current track
-            if(GPIO.input(sherlock.fw_pin) == GPIO.LOW):
-                if(time.time() - t_old > sherlock.bounce):
-                    GPIO.output(sherlock.fw_led_pin, GPIO.HIGH)
-                    start = time.time()
-                    long_press_flag = False
-                    while(GPIO.input(sherlock.fw_pin) == GPIO.LOW):
-                        duration = time.time() - start
-                        if(duration > sherlock.long_press_time):
-                            sherlock._fastforward()
-                            long_press_flag = True
-                            duration = 0
-                            start = time.time()
-
-                    if(not long_press_flag):    
-                        sherlock._forward()
-                    GPIO.output(sherlock.fw_led_pin, GPIO.LOW)
-                    t_old = time.time()
-            
-            # Backward press = restart track if after n seconds from start
-            # Backward press = previous track if before n seconds from start
-            if(GPIO.input(sherlock.bw_pin) == GPIO.LOW):
-                if(time.time() - t_old > sherlock.bounce):
-                    GPIO.output(sherlock.bw_led_pin, GPIO.HIGH)
-                    while(GPIO.input(sherlock.bw_pin) == GPIO.LOW):
-                        pass
-
-                    sherlock._backward()
-                    GPIO.output(sherlock.bw_led_pin, GPIO.LOW)
-                    t_old = time.time()   
-            
-            # Play button press = pause/play track
-            if(GPIO.input(sherlock.play_pin) == GPIO.LOW):
-                if(time.time() - t_old > sherlock.bounce):
-                    GPIO.output(sherlock.play_led_pin, GPIO.HIGH)
-                    while(GPIO.input(sherlock.play_pin) == GPIO.LOW):
-                        pass
-
-                    sherlock._play_pause()
-                    GPIO.output(sherlock.play_led_pin, GPIO.LOW)
-                    t_old = time.time()   
-
-            # Light switch press = turn on/off light/lamp
-            if(GPIO.input(sherlock.switch_pin) == GPIO.LOW):
-                if(time.time() - t_old > sherlock.bounce):
-                    sherlock._lamp_switch()
-                    while(GPIO.input(sherlock.switch_pin) == GPIO.LOW):
-                        pass
-                    t_old = time.time()
+        
+        # Start playback
+        sherlock.run(logger)
 
     # Catch CTRL+C command from CLI
     except KeyboardInterrupt:
         # Say goodbye and exit
-        goodbye_sherlock(logger)
+        goodbye_sherlock(False, logger)
     # Catch any other exception gracefully
     except Exception as ex:
         logger.info(f"Encountered the following error: {ex}.")
-        logger.info(f"Closing Sherlock session.")
+        logger.info(f"Closing Sherlock session...")
+        goodbye_sherlock(True, logger)
 
 
 if __name__=="__main__":
